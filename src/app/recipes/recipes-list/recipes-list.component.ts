0418bb2a0d437e62410d8de09c9fa8a711da51db
Ivan <ivan.palii@outlook.com>
2018-12-27 17:28:36 +0200

    Add html to components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/reciple.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipleas: Recipe[] = [
+    new Recipe('Forgotten  Realams', 'Fantasy',
+     'https://vignette.wikia.nocookie.net' +
+     '/rpg/images/e/e1/Forgotten_Realms_Campaign' +
+     '_Set.jpg/revision/latest?cb=20181029100324&path-prefix=ru')
+  ];
   constructor() { }
 
   ngOnInit() {
