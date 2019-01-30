39062aab80bda15d68aa40c86a11367eb03dd610
Ivan <ivan.palii@outlook.com>
2019-01-30 18:12:24 +0200

    Add router subscriber

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,23 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
-  users = [
-    {
-      id: 1,
-      name: 'Max'
-    },
-    {
-      id: 2,
-      name: 'Anna'
-    },
-    {
-      id: 3,
-      name: 'Chris'
-    }
-  ];
+export class UsersComponent implements OnInit {
+user: {id: number, name: string};
+
+/**
+ *
+ */
+constructor(private route: ActivatedRoute) {}
+
+ ngOnInit() {
+
+ }
+
 }
