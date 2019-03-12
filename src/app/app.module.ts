892a2f91ef7913b6e9808eb6a5d6b479cbd0948a
Ivan <ivan.palii@outlook.com>
2019-03-12 17:29:29 +0200

    Prepare new Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
-import { RoutingModule } from './routing/routing.module';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { AuthService } from './auth.service';
-import { AuthGuardService } from './auth-guard.service';
-import { ErrorPageComponent } from './error-page/error-page.component';
-import { ServerResolver } from './servers/server/server-resolver.service';
-
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent,
-    PageNotFoundComponent,
-    ErrorPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    RoutingModule
+    FormsModule
   ],
-  providers: [ServersService, AuthService, AuthGuardService, ServerResolver],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
